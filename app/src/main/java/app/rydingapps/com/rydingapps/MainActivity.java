dd54b98efcf74be072dac491bfeacdc11b926bdd
Rydingapps <rydingapps@gmail.com>
2016-02-12 21:20:24 +0100

    tessst

---
+++ app/src/main/java/app/rydingapps/com/rydingapps/MainActivity.java
@@ -0,0 +1,13 @@
+package app.rydingapps.com.rydingapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
